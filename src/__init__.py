305f166239b77dacef8699c3b602bf3d47a03766
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-06-10 16:13:56 +0200

    Initialize the app and register the blueprints

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
+
+    from src.api.text import text_blueprint
+
+    app.register_blueprint(text_blueprint)
+
+    return app
