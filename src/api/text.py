d2b55793d7aa7baa3803665bc92ec74ecb879ac3
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-06-11 11:26:10 +0200

    Change due to the isort changes

--- src/api/text.py
+++ src/api/text.py
@@ -1,9 +1,8 @@
+import pytesseract
 from flask import Blueprint, request
 from flask_restx import Api, Resource
 from PIL import Image
-import pytesseract
 from pytesseract import Output
-
 
 ocr_blueprint = Blueprint("ocr", __name__)
 api = Api(ocr_blueprint)
