1f1167833e6ccc1a35875e4a78b71211e021c504
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-06-11 11:21:09 +0200

    Change due to the black formatting

--- src/api/text.py
+++ src/api/text.py
@@ -11,24 +11,28 @@ api = Api(ocr_blueprint)
 
 class Ocr(Resource):
     def post(self):
-        file = request.files['file']
+        file = request.files["file"]
         image = Image.open(file.stream)
 
-        image = image.convert('L')
+        image = image.convert("L")
 
         boxes = pytesseract.image_to_data(image, output_type=Output.DICT)
 
         response_object = []
-        n_boxes = len(boxes['text'])
+        n_boxes = len(boxes["text"])
         for i in range(n_boxes):
-            if int(boxes['conf'][i]) > 60:
-                if boxes['text'][i] and boxes['text'][i] != " ":
-                    response_object.append({"left": boxes['left'][i],
-                                            "top": boxes['top'][i],
-                                            "right": boxes['left'][i] + boxes['width'][i],
-                                            "bottom": boxes['top'][i] + boxes['height'][i],
-                                            "text": boxes['text'][i]})
+            if int(boxes["conf"][i]) > 60:
+                if boxes["text"][i] and boxes["text"][i] != " ":
+                    response_object.append(
+                        {
+                            "left": boxes["left"][i],
+                            "top": boxes["top"][i],
+                            "right": boxes["left"][i] + boxes["width"][i],
+                            "bottom": boxes["top"][i] + boxes["height"][i],
+                            "text": boxes["text"][i],
+                        }
+                    )
         return response_object, 200
 
 
-api.add_resource(Ocr, '/ocr')
+api.add_resource(Ocr, "/ocr")
