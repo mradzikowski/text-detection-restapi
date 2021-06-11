cadb6a5a1751f421a2840c805fddc7933e032009
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-06-11 10:26:06 +0200

    Add restful route for posting images and retrieving information about points and detected text

--- src/api/text.py
+++ src/api/text.py
@@ -1,16 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restx import Api, Resource
-
-text_blueprint = Blueprint("ocr", __name__)
-api = Api(text_blueprint)
-
-
-class Text(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'text!'
-        }
+from PIL import Image
+import pytesseract
+from pytesseract import Output
 
 
-api.add_resource(Text, '/ocr')
+ocr_blueprint = Blueprint("ocr", __name__)
+api = Api(ocr_blueprint)
+
+
+class Ocr(Resource):
+    def post(self):
+        file = request.files['file']
+        image = Image.open(file.stream)
+
+        image = image.convert('L')
+
+        boxes = pytesseract.image_to_data(image, output_type=Output.DICT)
+
+        response_object = []
+        n_boxes = len(boxes['text'])
+        for i in range(n_boxes):
+            if int(boxes['conf'][i]) > 60:
+                if boxes['text'][i] and boxes['text'][i] != " ":
+                    response_object.append({"left": boxes['left'][i],
+                                            "top": boxes['top'][i],
+                                            "right": boxes['left'][i] + boxes['width'][i],
+                                            "bottom": boxes['top'][i] + boxes['height'][i],
+                                            "text": boxes['text'][i]})
+        return response_object, 200
+
+
+api.add_resource(Ocr, '/ocr')
